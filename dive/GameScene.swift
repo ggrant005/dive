7948a624ffd9960bf2bba8524f836a30886aa254
Greg Grant <ggrant005@gmail.com>
2018-07-17 20:44:08 -0700

    Gyro data moves triangle.

--- dive/GameScene.swift
+++ dive/GameScene.swift
@@ -6,105 +6,107 @@
 //  Copyright © 2018 Dumb Cheap Games. All rights reserved.
 //
 
+import CoreMotion
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+  
+  private var mTriangle : SKShapeNode?
+  
+  private var mMotion = CMMotionManager()
+  
+  private var mTimer : Timer?
+  
+  private var mRoll = 0.0
+  
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  override func sceneDidLoad() {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    startGyros()
+    createTriangle()
+  }
+  
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  func createTriangle() {
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let base = 90
+    let height = base * 2
     
-    override func sceneDidLoad() {
+    let path = UIBezierPath()
+    path.move(to: CGPoint(x: 0, y: 0))
+    path.addLine(to: CGPoint(x: -base / 2, y: height))
+    path.addLine(to: CGPoint(x: base / 2, y: height))
+    path.close()
+    
+    self.mTriangle = SKShapeNode.init(path: path.cgPath)
+    self.mTriangle!.strokeColor = SKColor.green
+    self.addChild(self.mTriangle!)
+  }
+  
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  func startGyros() {
+    
+    if self.mMotion.isGyroAvailable {
+      self.mMotion.gyroUpdateInterval = 1.0 / 60.0
+      self.mMotion.startGyroUpdates()
+      
+      // Configure a timer to fetch the accelerometer data.
+      self.mTimer = Timer(fire: Date(), interval: (1.0/60.0),
+                         repeats: true, block: { (timer) in
+                          
+                          // Get the gyro data.
+                          if let data = self.mMotion.gyroData {
+                            //let pitch = data.rotationRate.x
+                            self.mRoll = data.rotationRate.y
+                            //let yaw = data.rotationRate.z
+                          }
+      })
+      
+      // Add the timer to the current run loop.
+      RunLoop.current.add(self.mTimer!, forMode: .defaultRunLoopMode)
+    }
+  }
+  
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  func stopGyros() {
+    
+    if self.mTimer != nil {
+      self.mTimer?.invalidate()
+      self.mTimer = nil
+      
+      self.mMotion.stopGyroUpdates()
+    }
+  }
+  
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  func tiltTriangle() {
+    
+    let Roll = CGFloat(mRoll)
+    let tilt = SKAction.rotate(byAngle: Roll * -.pi/180 / 10, duration: 0.1)
+    let translate = SKAction.moveBy(x: Roll * 1.3, y: 0, duration: 0.1)
+    
+    let move = SKAction.sequence([tilt, translate])
+    let loop = SKAction.repeatForever(move)
+    
+    // stop shaking
+    // set horizontal to 0 roll
+    
+    
+    
+    mTriangle!.run(loop)
+  }
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
-    }
+    tiltTriangle()
+  }
 }
